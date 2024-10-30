507daf539f3f4f0d1622de4ce8f4c1ab580d058d
Titon Barua <titanix88@gmail.com>
2024-10-30 02:39:58 -0400

    Implemented graphs for problem 2.a

--- homework_2/one_layer_classifier.py
+++ homework_2/one_layer_classifier.py
@@ -93,8 +93,72 @@ def plot_epoch_vs_bce_error_graph(
     plt.show(block=True)
 
 
+DB_AXIS_POINTS = 100
+decision_boundary_grid = np.meshgrid(
+    np.linspace(-2.0, 2.0, DB_AXIS_POINTS),
+    np.linspace(-2.0, 2.0, DB_AXIS_POINTS))
+
+
+def plot_decision_boundaries(epoch_vs_decision_boundary, filepath=None):
+    rows = len(epoch_vs_decision_boundary)
+    cols = 2
+
+    fig, axes = plt.subplots(
+        rows, cols, figsize=(cols * 3 + 0.25, rows * 3 + 0.25 * 2))
+    fig.suptitle("Decision Boundaries", fontsize=10)
+
+    for i in range(rows):
+        epoch, X, Y = epoch_vs_decision_boundary[i]
+        GRID_X1 = X[:, 0].reshape(DB_AXIS_POINTS, DB_AXIS_POINTS)
+        GRID_X2 = X[:, 1].reshape(DB_AXIS_POINTS, DB_AXIS_POINTS)
+
+        TX1 = training_X[:, 0]
+        TX2 = training_X[:, 1]
+
+        for j in range(cols):
+            GRID_Z = Y[:, j].reshape(DB_AXIS_POINTS, DB_AXIS_POINTS)
+            ax = axes[i][j]
+            ax.set_aspect('equal')
+            ax.set_xlabel("x1")
+            ax.set_ylabel("x2")
+            ax.set_title(
+                "epoch: {}, var: y{}"
+                .format(epoch, j + 1), fontsize=8)
+
+            # Fill different decision areas and draw boundary.
+            ax.contourf(
+                GRID_X1, GRID_X2, GRID_Z,
+                colors=["tomato", "chartreuse", "chartreuse"],
+                levels=[0.0, 0.50, 1.0],
+                alpha=0.5)
+            ax.contour(
+                GRID_X1, GRID_X2, GRID_Z,
+                colors=["black"],
+                levels=[0.5],
+                alpha=0.75)
+
+            TY = training_Y[:, j]
+            TY_pos = np.argwhere(TY == 1)
+            TY_neg = np.argwhere(TY == 0)
+            ax.scatter(
+                TX1[TY_pos], TX2[TY_pos],
+                color="green", marker="^", label="1")
+            ax.scatter(
+                TX1[TY_neg], TX2[TY_neg],
+                color="red", marker="x", label="0")
+            ax.legend(loc="lower right")
+
+    fig.tight_layout()
+
+    if filepath:
+        fig.savefig(filepath)
+
+    plt.show()
+
+
 epoch_vs_bce_error = []
 epoch_vs_accuracy = []
+epoch_vs_decision_boundary = []
 
 
 def snapshot_callback(network, props):
@@ -103,6 +167,14 @@ def snapshot_callback(network, props):
     bce_error = bce_loss(training_Y, pred_Y)
     epoch_vs_bce_error.append((epoch, bce_error))
     epoch_vs_accuracy.append((epoch, props["accuracy"]))
+
+    if epoch in (3, 10, 100):
+        x1v = decision_boundary_grid[0].reshape(-1, 1)
+        x2v = decision_boundary_grid[1].reshape(-1, 1)
+        X = np.hstack((x1v, x2v))
+        Y = network.infer_batch(X)
+        epoch_vs_decision_boundary.append((epoch, X, Y))
+
 
 
 train_network(
@@ -118,3 +190,7 @@ plot_epoch_vs_bce_error_graph(
     epoch_vs_bce_error,
     epoch_vs_accuracy,
     "2.a-epoch_vs_bce_error.pdf")
+
+plot_decision_boundaries(
+    epoch_vs_decision_boundary,
+    "2.a-decision_boundaries.pdf")
